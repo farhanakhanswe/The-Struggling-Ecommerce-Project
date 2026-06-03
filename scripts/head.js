b9fee0bfe3eec7b63614c0c6cdc9a5aadc316653
Farhana <farhanakhanswe@gmail.com>
2026-06-03 22:58:10 +0600

    feat: create head, header and nav components

---
+++ scripts/head.js
@@ -0,0 +1,9 @@
+let head = document.getElementById("head");
+
+head.innerHTML = `
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/style.css">
+  <link rel="stylesheet" href="../styles/icons.css">
+  <title>Ecommerce</title>
+`;
---
+++ scripts/navbar.js
@@ -0,0 +1,8 @@
+let navbar = document.getElementById("navbar");
+
+navbar.innerHTML =`
+    <a href="index.html"><h1>Home</h1></h1></a>
+    <a href="about.html"><h1>About</h1></a>
+    <a href="contact.html"><h1>Contact</h1></a>
+`;
+
